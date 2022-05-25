ab908f9ae7a3955479c02ed3de81b11bea28bd0f
Yang Sukhun <69968627+Sagit25@users.noreply.github.com>
2022-05-25 21:29:31 +0900

    Update 2-1.ml

--- Programming Language/Homework2/2-1.ml
+++ Programming Language/Homework2/2-1.ml
@@ -5,4 +5,3 @@ let rec iter ((n, f) : int * ('a -> 'a)) : ('a -> 'a) =
     if n <= 0 then function x -> x
     else let comp ((f, g) : ('a -> 'a) * ('a -> 'a)) = function x -> f(g(x)) 
         in comp (f, iter(n-1, f))
-        (* Reference : https://ocaml.org/manual/coreexamples.html *)
